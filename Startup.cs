72aebc8c66c2181e682b3fbad2d00ef51d5fbe89
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2021-09-16 22:27:51 +0200

    Create loggin form

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace Chat_Online
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Loggin}/{action=Index}");
             });
         }
     }
